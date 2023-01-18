83e2cfcefb417f05534d110776d695d716c42c33
BlockArchitech <hello@blockarchitech.com>
2023-01-18 10:16:07 -0500

    ran prettier (spaces, 2), added generic oauth support

--- index.js
+++ index.js
@@ -1,34 +1,106 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser')
-var path = require ('path');
-const { syncBuiltinESMExports } = require('module');
-var sites = []
+var express = require("express");
+var passport = require("passport");
+var util = require("util");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var GitHubStrategy = require("passport-github2").Strategy;
+var partials = require("express-partials");
+var path = require("path");
+var sites = [];
+var GITHUB_CLIENT_ID = "c42cb6b28105c1423102";
+var GITHUB_CLIENT_SECRET = "3041032d8ebdaf15963a40856416dddc8d4179ea";
+
+// MIDDLEWARES
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(methodOverride());
+app.use(
+  session({
+    secret:
+      "nbgvfxdcgddfgfhgfhgjfdgjhkbkjlklmljknljhgvjgb876xdfs7t&T&BRB%&rbxZ&R7t8t",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(partials());
+// user serials
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://127.0.0.1:3000/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        // insert epic json db here
+        return done(null, profile);
+      });
+    }
+  )
+);
 
-function findSite(id) { 
-	var found = sites.filter(function(item) { return item.id === id; });
-	return found[0]
-}
-
-app.get('/', function(req, res) {
-  res.render('index');
+app.get("/", function (req, res) {
+  res.render("index", { user: req.user });
 });
 
-app.get('/post/:id', function(req, res) {
-  res.render('site', { site: findSite(req.params.id) });
+app.get("/site/:id", function (req, res) {
+  res.send("not yet");
 });
-app.post('/api/new/site', function(req, res) {
-		sites.push(req.body)
-		res.status(201).json({"msg": "created new website :tada:", "id": req.body.id})
-	
-})
-app.post('/api/update/site', function (req, res) {
-	var siteIndex = sites.findIndex((obj => obj.id == req.body.id));
-	sites[siteIndex].content = req.body.content
-	res.status(201).json({"msg": `updated site ${req.body.id} #${siteIndex}`})
-})
+app.post("/api/new/site", function (req, res) {
+  res.status(201).json({ msg: "not implemented" });
+});
+app.post("/api/update/site", function (req, res) {
+  // var siteIndex = sites.findIndex((obj => obj.id == req.body.id));
+  // sites[siteIndex].content = req.body.content
+  res.status(201).json({ msg: `not implemented` });
+});
+
+app.get("/account", ensureAuthenticated, function (req, res) {
+  res.render("account", { user: req.user });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login", { user: req.user });
+});
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  function (req, res) {}
+);
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 app.listen(3000);
-console.log('znci-web listening on 3000');
+console.log("znci-web listening on 3000");
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
