c9a78b252f8f4a3be4ec442c1b708ad7f1a76554
BlockArchitech <hello@blockarchitech.com>
2023-07-17 19:07:19 -0400

    changes
    Co-authored-by: Cerq <cerqiest@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**"
+      }
+    ]
+  }
+};
+module.exports = nextConfig;
