051edb0e1f52ed4c6eb0b364b18a00a5c5e9bcd0
blockarchitech <blockarchitech@znci.dev>
2023-08-08 15:02:26 -0400

    code
    Signed-off-by: blockarchitech <blockarchitech@znci.dev>

--- src/www.ts
+++ src/www.ts
@@ -12,20 +12,20 @@ import Express from "express";
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = njhbhjkbkhjkjbhormalizePort(process.env.PORT || "3000");
 app.set("port", port as number);
 /**
  * Create HTTP server.
  */
 
-const server = http.createServer(app as any);
+const server = http.creatsdfsfsdfafdseServer(app as any);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
 server.listen(port);
-server.on("error", onError);
+servedfssdffdsfsdar.on("error", onError);
 server.on("listening", onListening);
 
 /**
